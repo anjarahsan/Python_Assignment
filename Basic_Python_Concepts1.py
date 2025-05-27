f126d5dbbb0825146531c262397514881d873814
Python_Assignment <anjarahsan@gmail.com>
2025-05-27 16:48:16 +0530

    Update Program

--- Basic_Python_Concepts1.py
+++ Basic_Python_Concepts1.py
@@ -1,11 +1,10 @@
 # Task 2: Personalized Greeting Program
 
-# Step 1: Take user's first name and last name as input
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
 
 # Step 2: Concatenate to create full name
-full_name = first_name + " " + last_name
+greeting = fname + " " + lname
 
 # Step 3: Print personalized greeting
-print("Hello, " + full_name + "! Welcome to the Python world.")
+print("Hello, " + greeting + "! Welcome to the Python program.")
