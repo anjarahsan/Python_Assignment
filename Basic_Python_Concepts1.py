491c6fabdd72dee6fd3d27d8de39b798b64c8ef0
Python_Assignment <anjarahsan@gmail.com>
2025-05-27 16:49:59 +0530

    Update Program

--- Basic_Python_Concepts1.py
+++ Basic_Python_Concepts1.py
@@ -2,9 +2,5 @@
 
 fname = input("Enter your first name: ")
 lname = input("Enter your last name: ")
-
-# Step 2: Concatenate to create full name
 greeting = fname + " " + lname
-
-# Step 3: Print personalized greeting
 print("Hello, " + greeting + "! Welcome to the Python program.")
