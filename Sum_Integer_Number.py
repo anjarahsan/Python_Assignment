9f73d3691b7bcc7a7e488f3beb604f86da3b9645
Python_Assignment <anjarahsan@gmail.com>
2025-05-29 12:19:05 +0530

    Updated Sum_Integer_Number

--- Sum_Integer_Number.py
+++ Sum_Integer_Number.py
@@ -1,5 +1,4 @@
-n=int(input("Enter 1 to 50 number:"))
-sum=0
-for i in range(n+1):
-    sum=sum+i
-print("Tthe sum of number from 1 to 50 is :",sum)
+sum = 0
+for i in range(1, 51):
+    sum = i+sum
+print("The sum of number from 1 to 50 is :",sum)
